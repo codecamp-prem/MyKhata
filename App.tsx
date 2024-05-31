64e2fb14d957a63e0dcb095434e94ee10ccb3732
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-05-31 14:35:47 +0545

    import existing sqlite db

--- App.tsx
+++ App.tsx
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from "expo-asset";
+import * as FileSystem from "expo-file-system";
+import { SQLiteProvider } from "expo-sqlite";
+import { Suspense, useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+
+const loadDatabase = async () => {
+  const dbName = "MyKhataAppSqlite.db";
+  const dbAsset = require("./assets/MyKhataAppSqlite.db");
+  const dbUri = Asset.fromModule(dbAsset).uri;
+  const dbFilePath = `${FileSystem.documentDirectory}SQLite/${dbName}`;
+
+  const fileInfo = await FileSystem.getInfoAsync(dbFilePath);
+  if (!fileInfo.exists) {
+    await FileSystem.makeDirectoryAsync(
+      `${FileSystem.documentDirectory}SQLite`,
+      { intermediates: true }
+    );
+    await FileSystem.downloadAsync(dbUri, dbFilePath);
+  }
+};
 
 export default function App() {
+  const [dbLoaded, setDbLoaded] = useState(false);
+
+  useEffect(() => {
+    loadDatabase()
+      .then(() => setDbLoaded(true))
+      .catch((e) => console.error(e));
+  }, []);
+
+  if (!dbLoaded) {
+    return (
+      <View style={{ flex: 1 }}>
+        <ActivityIndicator size={"large"} />
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Suspense>
+      <SQLiteProvider
+        databaseName="MyKhataAppSqlite.db"
+        useSuspense
+      ></SQLiteProvider>
+    </Suspense>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
