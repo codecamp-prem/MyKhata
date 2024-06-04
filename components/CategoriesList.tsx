faad60b2ab0ba7a189292bed2de93e1fbbece698
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-06-04 11:49:58 +0545

    confirmation alert for deleteing category

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity, View } from "react-native";
+import { Alert, TouchableOpacity, View } from "react-native";
 import { Categories } from "../types";
 import CategoryListItem from "./CategoryListItem";
 
@@ -10,6 +10,25 @@ const CategoriesList = ({
   catergories: Categories[];
   deleteCategory: (id: number) => Promise<void>;
 }) => {
+  const handleShowAlert = (category_id: number) => {
+    Alert.alert(
+      "Confirmation",
+      "Do you want to Delete this category?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {},
+          style: "cancel",
+        },
+        {
+          text: "Yes",
+          onPress: () => deleteCategory(category_id),
+        },
+      ],
+      { cancelable: true }
+    );
+  };
+
   return (
     <View>
       {catergories.map((category) => {
@@ -17,7 +36,7 @@ const CategoriesList = ({
           <TouchableOpacity
             key={category.id}
             activeOpacity={0.7}
-            onLongPress={() => deleteCategory(category.id)}
+            onLongPress={() => handleShowAlert(category.id)}
           >
             {/* category item */}
             <CategoryListItem category={category} />
