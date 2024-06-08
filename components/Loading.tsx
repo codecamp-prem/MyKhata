a63a5856d36788610393eeb6400ca288ec742fa7
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2024-06-08 12:29:36 +0545

    change splash screen and app icon

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,12 +1,23 @@
-import { ActivityIndicator, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Loading = () => {
   return (
-    <View style={{ flex: 1, backgroundColor: "red" }}>
-      <ActivityIndicator size={"large"} />
-      <Text>Loading...</Text>
+    <View style={styles.statusContainer}>
+      <Text style={styles.statusText}>Loading....</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  statusContainer: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: "white",
+    borderRadius: 5,
+  },
+  statusText: {
+    color: "#333",
+    textAlign: "center",
+  },
+});
 export default Loading;
